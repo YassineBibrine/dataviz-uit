fd8b54255bd755b1a9fdc2a26a8ab43cd084eba5
abir11zh <abirzahraoui64@gmail.com>
2025-11-23 19:58:22 +0100

    Ajout AlgorithmFactory & AlgorithmManager

--- src/orchestration/algorithm_factory.h
+++ src/orchestration/algorithm_factory.h
@@ -2,22 +2,20 @@
 
 #include <string>
 #include <map>
-#include <vector>
 
 class AlgorithmRunner; // fwd
 
 struct AlgorithmInfo {
- std::string name;
- std::string description;
- std::string category;
- std::vector<std::string> supportedStructures;
+    std::string name;
+    std::string description;
+    std::string category; // Utile pour AlgorithmManager, même si non dans l'interface
 };
 
 class AlgorithmFactory {
 public:
- virtual ~AlgorithmFactory() = default;
- virtual AlgorithmRunner* create(const std::map<std::string,std::string>& params) =0;
- virtual std::string getName() const =0;
- virtual std::string getDescription() const =0;
- virtual std::string getCategory() const { return "General"; }
+    virtual ~AlgorithmFactory() = default;
+
+    virtual AlgorithmRunner* create(const std::map<std::string, std::string>& params) = 0;
+    virtual std::string getName() const = 0;
+    virtual std::string getDescription() const = 0;
 };
